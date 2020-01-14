612c4d578ab4ff5f75a9a35bca9e68679435cb29
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2020-01-14 07:56:04 -0700

    Add project files.

---
+++ RazorPagesBootstrapPopoverDemo/Pages/Popover.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesBootstrapPopoverDemo
+{
+    public class PopoverModel : PageModel
+    {
+        public List<string> Messages { get; set; }
+        public void OnGet()
+        {
+            Messages = new List<string>()
+            {
+                "Turns out NASA can't even improve on duct tape... Duct tape is magic and should be worshipped.",
+                "How come Aquaman can control whales? They're mammals! Makes no sense.",
+                "As with most of life's problems, this one can be solved by a box of pure radiation."
+            };
+        }
+    }
+}
